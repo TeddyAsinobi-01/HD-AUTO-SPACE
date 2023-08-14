01b8c8efd51e4d0c217599831bfbbc6baf1b0ac5
Teddie <ibonisaledu@gmail.com>
2023-08-14 20:06:12 +0100

    engine parts added

--- product.js
+++ product.js
@@ -1,5 +1,9 @@
+// PAGINATION FOR FILTERS
+
 var btn = document.getElementsByClassName("btn");
 var slide = document.getElementById("slide");
+
+
 btn[0].onclick = function () {
     slide.style.transform = "translatex(0px)";
     for (i = 0; i < 4; i++) {
@@ -25,6 +29,38 @@ btn[3].onclick = function () {
     slide.style.transform = "translatex(-2700px)";
     for (i = 0; i < 4; i++) {
         btn[i].classList.remove("active");
+    }
+    this.classList.add("active");
+}
+// PAGINATION FOR ENGINE PARTS
+var btn2 = document.getElementsByClassName("btn2");
+var slide2 = document.getElementById("slide2");
+
+btn2[0].onclick = function () {
+    slide2.style.transform = "translatex(0px)";
+    for (i = 0; i < 4; i++) {
+        btn2[i].classList.remove("active");
+    }
+    this.classList.add("active");
+}
+btn2[1].onclick = function () {
+    slide2.style.transform = "translatex(-870px)";
+    for (i = 0; i < 4; i++) {
+        btn2[i].classList.remove("active");
+    }
+    this.classList.add("active");
+}
+btn2[2].onclick = function () {
+    slide2.style.transform = "translatex(-1800px)";
+    for (i = 0; i < 4; i++) {
+        btn2[i].classList.remove("active");
+    }
+    this.classList.add("active");
+}
+btn2[3].onclick = function () {
+    slide2.style.transform = "translatex(-2700px)";
+    for (i = 0; i < 4; i++) {
+        btn2[i].classList.remove("active");
     }
     this.classList.add("active");
 }
