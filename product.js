e7293784b2ca980e7f9e85711d72618e5bb09a9c
Teddie <ibonisaledu@gmail.com>
2023-08-17 20:17:48 +0100

    ballons and electrical

--- product.js
+++ product.js
@@ -39,35 +39,35 @@ var slide2 = document.getElementById("slide2");
 
 btn2[0].onclick = function () {
     slide2.style.transform = "translatex(0px)";
-    for (i = 0; i < 4; i++) {
+    for (i = 0; i < 5; i++) {
         btn2[i].classList.remove("active");
     }
     this.classList.add("active");
 }
 btn2[1].onclick = function () {
     slide2.style.transform = "translatex(-870px)";
-    for (i = 0; i < 4; i++) {
+    for (i = 0; i < 5; i++) {
         btn2[i].classList.remove("active");
     }
     this.classList.add("active");
 }
 btn2[2].onclick = function () {
     slide2.style.transform = "translatex(-1800px)";
-    for (i = 0; i < 4; i++) {
+    for (i = 0; i < 5; i++) {
         btn2[i].classList.remove("active");
     }
     this.classList.add("active");
 }
 btn2[3].onclick = function () {
     slide2.style.transform = "translatex(-2700px)";
-    for (i = 0; i < 4; i++) {
+    for (i = 0; i < 5; i++) {
         btn2[i].classList.remove("active");
     }
     this.classList.add("active");
 }
 btn2[4].onclick = function () {
     slide2.style.transform = "translatex(-3600px)";
-    for (i = 0; i < 4; i++) {
+    for (i = 0; i < 5; i++) {
         btn2[i].classList.remove("active");
     }
     this.classList.add("active");
@@ -79,22 +79,48 @@ var slide3 = document.getElementById("slide3");
 
 btn3[0].onclick = function () {
     slide3.style.transform = "translatex(0px)";
-    for (i = 0; i < 4; i++) {
+    for (i = 0; i < 3; i++) {
         btn3[i].classList.remove("active");
     }
     this.classList.add("active");
 }
 btn3[1].onclick = function () {
     slide3.style.transform = "translatex(-870px)";
-    for (i = 0; i < 4; i++) {
+    for (i = 0; i < 3; i++) {
         btn3[i].classList.remove("active");
     }
     this.classList.add("active");
 }
 btn3[2].onclick = function () {
     slide3.style.transform = "translatex(-1800px)";
-    for (i = 0; i < 4; i++) {
+    for (i = 0; i < 3; i++) {
         btn3[i].classList.remove("active");
+    }
+    this.classList.add("active");
+}
+
+//PAGINATION FOR ELECTRICALS
+var btn4 = document.getElementsByClassName("btn4");
+var slide4 = document.getElementById("slide4");
+
+btn4[0].onclick = function () {
+    slide4.style.transform = "translatex(0px)";
+    for (i = 0; i < 3; i++) {
+        btn4[i].classList.remove("active");
+    }
+    this.classList.add("active");
+}
+btn4[1].onclick = function () {
+    slide4.style.transform = "translatex(-870px)";
+    for (i = 0; i < 3; i++) {
+        btn4[i].classList.remove("active");
+    }
+    this.classList.add("active");
+}
+btn4[2].onclick = function () {
+    slide4.style.transform = "translatex(-1800px)";
+    for (i = 0; i < 3; i++) {
+        btn4[i].classList.remove("active");
     }
     this.classList.add("active");
 }
